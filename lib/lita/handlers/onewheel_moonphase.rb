4fcd43a98b3d6c12bfe2fdbbe192ce81ee34c0b3
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-17 23:28:59 -0800

    Killing sample code.

--- lib/lita/handlers/onewheel_moonphase.rb
+++ lib/lita/handlers/onewheel_moonphase.rb
@@ -29,24 +29,3 @@ module Lita
     end
   end
 end
-{
-    "year": 2016,
-    "month": 11,
-    "day": 18,
-    "dayOfWeek": "Friday",
-    "lng": -122.186048,
-    "lat": 45.205518,
-    "tz": -8,
-    "sundata": {
-        "civilRise": "06:40",
-        "rise": "07:12",
-        "transit": "11:54",
-        "set": "16:36",
-        "civilSet": "17:08"
-    },
-    "moondata": {
-        "transit": "03:45",
-        "set": "11:16",
-        "rise": "21:14"
-    }
-}
